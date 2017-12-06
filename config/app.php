817b3ba0f2d3e5e205f8ef1eb3ff35c29bbea056
Ivan Lim <aimprove@gmail.com>
2017-12-06 11:28:01 -0500

    Install Form service provider from Laravel Collective

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -227,6 +228,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
