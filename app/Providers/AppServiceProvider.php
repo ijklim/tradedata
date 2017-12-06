46bf1a595a0b78de51571f3a20b6f55cccd7a525
Ivan Lim <aimprove@gmail.com>
2017-12-06 18:34:36 -0500

    Add customized form component bootstrapText

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Register Bootstrap 4 Form elements
+        // 'components.form.text' refers to resources/views/components/form/text.blade.php
+        // e.g. Form::bootstrapText('symbol', null, ['placeholder' => 'symbol, e.g. QQQ'])
+        \Form::component('bootstrapText', 'components.form.text', ['name', 'value', 'attributes']);
     }
 
     /**
