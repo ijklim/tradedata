43ab0f3233dd4481fcfef877666506c3b9652ba4
Ivan Lim <aimprove@gmail.com>
2017-12-07 16:56:59 -0500

    Add error feedback text to alert

--- resources/views/components/form/text.blade.php
+++ resources/views/components/form/text.blade.php
@@ -1,4 +1,5 @@
 <div class="form-group">
     {{ Form::label($name, null) }}
-    {{ Form::text($name, $value, array_merge(['class' => 'form-control'], $attributes ?? [])) }}
+    {{ Form::text($name, $value, array_merge(['class' => 'form-control' . ($errors->has($name) ? ' is-invalid' : '')], $attributes ?? [])) }}
+    <small class="form-text text-danger">{{ $errors->first($name) }}</small>
 </div>
