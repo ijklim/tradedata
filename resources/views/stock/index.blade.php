d3034e9b818640da99ad998c7232d7a5b73a7314
Ivan Lim <aimprove@gmail.com>
2017-12-07 17:05:57 -0500

    Reposition buttons and add floating Add Stock button

--- resources/views/stock/index.blade.php
+++ resources/views/stock/index.blade.php
@@ -1,45 +1,73 @@
 @extends('layouts.app')
 
+@php
+    // Declare common variables
+    const PAGE_NAME = 'Stocks';
+    $itemName = substr(PAGE_NAME, 0, strlen(PAGE_NAME) - 1);
+    $items = $stocks ?? null;
+@endphp
+
 @section('content')
-    @if (isset($stocks))
-        @foreach ($stocks as $stock)
+    <h1 class="col-12 text-center">
+        {!! PAGE_NAME !!}
+    </h1>
+    <!-- Add Button -->
+    <button
+        class='btn rounded-circle button--bottom-float'
+        onclick='window.location = "/stock/create"'
+        title='Add {!! $itemName !!}';
+    >
+        +
+    </button>
+
+    @if (isset($items))
+        @foreach ($items as $item)
             <div class="col-sm-6 col-md-4 mt-3">
                 <div class='card border-dark'>
                     <div class="card-header text-white bg-dark">
-                        {{ $stock->symbol }}
+                        {{ $item->symbol }}
                     </div>
                     <div class="card-body">
-                        <div>{{ $stock->name }}</div>
-                        <div>Available Prices: {{ $stock->stockprices()->get()->count() }}</div>
-
-                        <div class='mt-2 text-center'>
+                        <div>{{ $item->name }}</div>
+                        <div>Available Prices: {{ $item->stockprices()->get()->count() }}</div>
+                    </div>
+                    <div class="card-footer">
+                        <div class='row'>
+                            <div class='col px-1'>
+                                <button
+                                    class='btn btn-primary btn-block'
+                                    onclick='window.location = "/stock-price/{{ $item->symbol }}"'
+                                >
+                                    Prices
+                                </button>
+                            </div>
+                            <div class='col px-1'>
                             <button
-                                class='btn btn-primary'
-                                onclick='window.location = "/stock-price/{{ $stock->symbol }}"'
-                            >
-                                Prices
-                            </button>
-                            <button
-                                class='btn btn-primary'
-                                onclick='window.location = "/stock/{{ $stock->symbol }}"'
+                                class='btn btn-primary btn-block'
+                                onclick='window.location = "/stock/{{ $item->symbol }}"'
                             >
                                 Show
                             </button>
+                            </div>
+                            <div class='col px-1'>
                             <button
-                                class='btn btn-primary'
-                                onclick='window.location = "/stock/{{ $stock->symbol }}/edit"'
+                                class='btn btn-primary btn-block'
+                                onclick='window.location = "/stock/{{ $item->symbol }}/edit"'
                             >
                                 Edit
                             </button>
+                            </div>
+                            <div class='col px-1'>
                             {{
                                 Form::open([
                                     'method' => 'DELETE',
-                                    'route' => ['stock.destroy', $stock->symbol],
+                                    'route' => ['stock.destroy', $item->symbol],
                                     'class' => 'd-inline'
                                 ])
                             }}
-                                {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
+                                {{ Form::submit('Delete', ['class' => 'btn btn-danger btn-block']) }}
                             {{ Form::close() }}
+                            </div>
                         </div>
                     </div>
                 </div>
