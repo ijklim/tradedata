03aa979e5ff5cf436a9078091ce190bfe691bb6d
Ivan Lim <aimprove@gmail.com>
2017-12-07 16:57:49 -0500

    Add model DataSource

---
+++ app/DataSource.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DataSource extends Model
+{
+    protected $fillable = [
+        'domain_name', 'api_base_url'
+    ];
+}
